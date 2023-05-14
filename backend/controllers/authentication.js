11dd4751522396477f0aea8aef47a7f6cb8c38be
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-05-14 11:27:59 -0700

    404 error

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -6,15 +6,30 @@ const bcrypt = require('bcrypt')
 const { User } = db
 
   
-router.post('/', async (req, res) => {
+// router.post('/', async (req, res) => {
 
+//     let user = await User.findOne({
+//         where: { email: req.body.email }
+//     })
+
+//     console.log(user)
+// })
+  
+  //compare the password we collected from the front end with the passwordDigest we have stored in our back end
+router.post('/', async (req, res) => {
+    
     let user = await User.findOne({
         where: { email: req.body.email }
     })
 
-    console.log(user)
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({ 
+            message: `Could not find a user with the provided username and password` 
+        })
+    } else {
+        res.json({ user })
+    }
 })
-  
 
 
 module.exports = router
