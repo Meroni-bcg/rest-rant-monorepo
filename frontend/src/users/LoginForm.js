653befd2073ab2ee127659dd2718d0ee766fe776
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-05-14 11:21:45 -0700

    fetch requesttrigger-routehandler login submission

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -17,8 +17,15 @@ function LoginForm() {
 
     async function handleSubmit(e) {
         e.preventDefault()
-       
-
+       const response = await fetch('https://localhost:3000/authentication', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+       })
+       const data = await response.json()
+       console.log(data)
     }
 
     return (
